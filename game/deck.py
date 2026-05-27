37f7fff31e7580d763f3030ee4a7aa55285304e2
Andrei <atberz06@gmail.com>
2026-05-27 16:19:33 -0400

    Deleted old card inteface (bad), and created new deck creator

---
+++ game/deck.py
@@ -0,0 +1,46 @@
+"""
+Module that defines the deck, and methods for manipulating a deck.
+"""
+import random
+
+# suits global var
+SUITS = {1: "club", 2: "diamond", 3: "heart", 4: "spade"}
+
+class Deck:
+    """
+    A deck of cards in poker.
+    A deck is a stack of cards - which are tuples of (value, suit).
+    value is the integer denomination of the card: Ace = 1, then all numbers, then J = 11, Q = 12, K = 13.
+    suit is one of {1, 2, 3, 4}, which maps to the corresponding suit in the SUITS global var above.
+
+    Instance Attributes:
+        - _cards: A list of the cards currently in the deck, with the last index corresponding to the topmost card.
+
+    Representation Invariants:
+        - len(self._cards) <= 52
+        - all(1 <= card[0] <= 13 for card in self._cards)
+        - all(card[1] in {1, 2, 3, 4} for card in self._cards)
+    """
+    _cards: list[tuple[int, int]]
+
+    def __init__(self, ordered: bool = False) -> None:
+        """
+        Initialize this deck of cards.
+        The ordered parameter determines whether the deck is generated in sorted order.
+        sorted order works as follows:
+            1. Clubs in ascending order (A -> K)
+            2. Diamonds in ascending order (A -> K)
+            3. Hearts in ascending order (A -> K)
+            4. Spades in ascending order (A -> K)
+            Jokers are excluded.
+        A random permutation of the deck is created if ordered = False.
+        """
+        self._cards = []
+        self._cards.extend((i, j) for j in range(1, 5) for i in range(1, 14))
+        if ordered:
+            return
+        else:
+            random.shuffle(self._cards)
+            return
+
+
